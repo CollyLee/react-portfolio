22bc03a2cee94eabed2c4099ff7be5dc62a5bc3a
CollyLee <clh5160@gmail.com>
2023-05-23 15:03:30 -0400

    wrote App.js, index.js, NavTabs.js, and PortfolioContainer.js

---
+++ src/components/NavTabs.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+        <ul className="nav nav-tabs">
+
+            {/* PORTFOLIO CHANGE HANDLER */}
+            <li className="nav-item">
+                <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+
+            {/* ABOUT CHANGE HANDLER */}
+            <li className="nav-item">
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                    About Me
+                </a>
+            </li>
+
+            {/* CONTACT CHANGE HANDLER */}
+            <li className="nav-item">
+                <a
+                    href="#contact"
+                    onClick={() => handlePageChange('Contact')}
+                    // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact Me
+                </a>
+            </li>
+
+            {/* RESUME CHANGE HANDLER */}
+            <li className="nav-item">
+                <a
+                    href="#resume"
+                    onClick={() => handlePageChange('Resume')}
+                    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    View My Resume
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default NavTabs;
